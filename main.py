0d59a9b751090d7d3ce38d00520db04519abcdf1
flick0 <flickerdroid211@gmail.com>
2022-08-02 18:47:55 +0530

    hide token

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import json
+
 from bot.bot import Bot
 
-TOKEN = "OTc3ODczOTU5OTkzMzY0NTAw.GyC5xc.i8dnoblDIaU2ZjCzDVNY8bUefGCBWXxwLFg-xw"
+with open("./config.json") as f:
+    config = json.load(f)
+    TOKEN = config["token"]
 
 if __name__ == "__main__":
     Bot().run(TOKEN)
